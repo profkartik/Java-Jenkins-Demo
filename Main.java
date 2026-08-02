120dc7f783df46c61a883b105a00b07215214f14
profkartik <kartik@somaiya.edu>
2026-08-02 17:23:36 +0530

    Updated Output Message

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from Jenkins Automation!");
+        System.out.println("Build triggered automatically on Git Push!");
     }
 }
