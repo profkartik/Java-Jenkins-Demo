665db226f80e9a796b04c33045466dd1604351a1
profkartik <kartik@somaiya.edu>
2026-08-09 14:02:29 +0530

    Remove duplicate print statement in Main.java

--- Main.java
+++ Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("Build triggered automatically on Git Push!");
         System.out.println("Build triggered automatically on Git Push-1!");
         System.out.println("Build triggered automatically on Git Push-1!");
+        System.out.println("Build triggered automatically on Git Push-1!");
     }
 }
